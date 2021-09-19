e8495e952ce0ba2dea98c68f45ccdd630e84da89
rijfas <67458012+rijfas@users.noreply.github.com>
2021-09-19 11:33:02 +0530

    Update main.py

--- joy_bot-spyderMonkey/main.py
+++ joy_bot-spyderMonkey/main.py
@@ -16,18 +16,21 @@ def command(message):
     if user_msg == '/start' or message == '/help':
       app.send_message(chat_dest, 'Send me a joy code and i will render the image for you!')
     else:
-      if os.environ["BUSY"] == "TRUE":
-          app.send_message(
-              chat_dest, "sorry working on a code right now, please wait some time!"
-          )
-      os.environ["BUSY"] = "TRUE"
-      joyBot.execute(user_msg)
-      image = open("output.png", "rb")
-      status = post(
-          f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_dest}",
-          files={"photo": image.read()},
-      )
-      os.environ["BUSY"] = "FALSE"
+      try:
+        if os.environ["BUSY"] == "TRUE":
+            app.send_message(
+                chat_dest, "sorry working on a code right now, please wait some time!"
+            )
+        os.environ["BUSY"] = "TRUE"
+        joyBot.execute(user_msg)
+        image = open("output.png", "rb")
+        status = post(
+            f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_dest}",
+            files={"photo": image.read()},
+        )
+        os.environ["BUSY"] = "FALSE"
+      except:
+        app.send_message(chat_dest, 'syntax error an enn thonnunnu! idk')
 
 
 if __name__ == "__main__":
